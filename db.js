ab779ea91623520c0ee617522a44c97d70013ba3
Jenny Oliva Vallejos <jaov669@gmail.com>
2025-05-18 21:04:25 -0500

    fix

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
-const mysql = require('mysql2/promise'); 
+// db.js
+require('dotenv').config();
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '360complemento',
-  database: 'registro_ganadero',
+  host:     process.env.MYSQLHOST     || process.env.DB_HOST,
+  user:     process.env.MYSQLUSER     || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port:     process.env.MYSQLPORT     || process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-
-
-
 module.exports = pool;
-
